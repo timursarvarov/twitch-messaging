6e1d50361e1299cfc7d941e0e37f4c9d269a44d3
timursarvarov <53015375+timursarvarov@users.noreply.github.com>
2023-06-07 08:53:21 +0300

    Feat Add chat module

---
+++ apps/server/src/chat/dto/add-message.dto.ts
@@ -0,0 +1,10 @@
+import { IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class AddMessageDto {
+  @IsString()
+  text: string;
+
+  @IsOptional()
+  @IsUUID()
+  userId?: string;
+}
